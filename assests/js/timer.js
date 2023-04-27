da55cae3107123f45d3789c88d92f6b24baa2492
= <=>
2023-04-27 08:39:19 +0200

    update all page

--- assests/js/timer.js
+++ assests/js/timer.js
@@ -24,6 +24,7 @@ window.fetch = async (...args) => {
 
     //append new header lang
     options.headers.append('Accept-Language', appLanguage);
+    options.headers.append('Accept', "application/json");
 
     //execute original fetch
     let response = await originalFetch(resource, options);
@@ -36,260 +37,368 @@ window.fetch = async (...args) => {
  */
 let quiz = {
 
-    /**
-     * @timer : HTMLElement => block global timer
-     */
-    $timer: HTMLElement,
+        /**
+         * @timer : HTMLElement => block global timer
+         */
+        $timer: HTMLElement,
 
-    /**
-     * @timerTxt : HTMLElement  => block text timer
-     */
-    $timerTxt: HTMLElement,
+        /**
+         * @timerTxt : HTMLElement  => block text timer
+         */
+        $timerTxt: HTMLElement,
 
-    /**
-     * @btnsAnswer : Array HTMLElement => btn trigger to answer quiz
-     */
-    $btnsAnswer: Array,
+        /**
+         * @btnsAnswer : Array HTMLElement => btn trigger to answer quiz
+         */
+        $btnsAnswer: Array,
 
-    /**
-     * @maxTime : maximum time limit  per seconde for one question
-     */
-    maxTime: 10,
+        $containerQuestion: HTMLElement,
 
-    /**
-     * @countTime : Number => seconde number under timer graph
-     */
-    countTime: 0,
+        /**
+         * @maxTime : maximum time limit  per seconde for one question
+         */
+        maxTime: 60,
 
-    /**
-     * @deg : Number => degree rotation for timer and arrow
-     */
-    deg: 0,
+        /**
+         * @countTime : Number => seconde number under timer graph
+         */
+        countTime: 0,
 
-    /**
-     * @idxAnswer Number | NULL => binding index response from button
-     */
-    idxAnswer: null,
+        /**
+         * @deg : Number => degree rotation for timer and arrow
+         */
+        deg: 0,
 
-    /**
-     * empty Array : questions for quiz
-     */
-    questions: [
-        {
-            "id": 24,
-            "text": "Lequel de ces sélecteurs a la spécificité la plus élevée ?",
-            "answers": [
-                {
-                    "text": "#content",
-                    "is_correct": true
-                },
-                {
-                    "text": ":first-child",
-                    "is_correct": false
-                },
-                {
-                    "text": "main",
-                    "is_correct": false
-                },
-                {
-                    "text": ".col",
-                    "is_correct": false
-                }
-            ]
-        }
-    ],
+        /**
+         * @idxAnswer Number | NULL => binding index response from button
+         */
+        idxAnswer: null,
 
-    /**
-     * @cat  : Array => content cat per default
-     */
-    categories: [],
-
-    /**
-     * @selectedCategories | number =>  binding from url _Get['cat']
-     */
-    selectedCategorie: Number,
-
-    /**
-     * reference on interval timer
-     */
-    refIntervalTimer: null,
-
-    /**
-     * Function init quiz
-     */
-    init: function () {
-
-        //set counter Time
-        this.countTime = this.maxTime;
-
-        //set  element html
-        this.$timer = document.querySelector('.timer-graphic');
-
-        this.$timerTxt = document.querySelector('.timer-seconde');
-
-        this.$btnsAnswer = document.querySelectorAll('.btn-answer');
-
-        //add listeners for for all buttons to  trigger answer
-        this.$btnsAnswer.forEach(($el, key) => {
-            $el.addEventListener('click', (event) => {
-                //get parent
-                const $parent = event.target.parentNode;
-                //get position
-                this.indexAnswer = Array.prototype.indexOf.call(parent.children, event.target);
-                this.stop_timer().check_answer();
-            })
-        })
-    },
-
-    /**
-     * function start timer and timer rotate
-     * @return  void
-     */
-    start_timer: function () {
-        this.refIntervalTimer = setInterval(() => {
-            this.countTime--;
-            this.deg += 360 / this.maxTime;
-            if (this.countTime >= 0) {
-                this.timer_rotate(this.$timer, this.deg);
-                this.$timerTxt.innerHTML = this.countTime + "S";
-                this.animateTimer(this.deg);
-            } else {
-                this.stop_timer().check_answer();
+        /**
+         * empty Array : questions for quiz
+         */
+        questions: [
+            {
+                "id": 24,
+                "text": "Lequel de ces sélecteurs a la spécificité la plus élevée ?",
+                "answers": [
+                    {
+                        "text": "#content",
+                        "is_correct": true
+                    },
+                    {
+                        "text": ":first-child",
+                        "is_correct": false
+                    },
+                    {
+                        "text": "main",
+                        "is_correct": false
+                    },
+                    {
+                        "text": ".col",
+                        "is_correct": false
+                    }
+                ]
             }
-        }, 1000);
-    },
+        ],
 
-    /**
-     *
-     * @param deg : number of degree from $timer
-     */
-    animateTimer: function (deg) {
-        if (deg > 45 && this.deg < 90) {
-            this.$timer.style.animation = "zoom-in-zoom-level-1 0.75s ease infinite";
-        } else if (deg > 90 && deg < 180) {
-            this.$timer.style.animation = "zoom-in-zoom-level-1 0.50s ease infinite";
-        } else if (deg > 180 && deg < 225) {
-            this.$timer.style.animation = "zoom-in-zoom-level-1 0.25s ease infinite";
-        } else if (deg > 260) {
-            let timeAnimation = (360 - deg) * (0.7 / 360) + 0.1;
-            this.$timer.style.animation = "zoom-in-zoom-level-1 " + timeAnimation + "s ease infinite";
-        } else {
-            return false;
+        /**
+         * @cat  : Array => content cat per default
+         */
+        categories: [],
+
+        /**
+         * @selectedCategories | number =>  binding from url _Get['cat']
+         */
+        selectedCategorie: Number,
+
+        /**
+         * @selectedQuestions index : Number
+         */
+        selectedQuestion: Number,
+
+        /**
+         * reference on interval timer
+         */
+        refIntervalTimer: null,
+
+        /**
+         * Function init quiz
+         */
+        init: function () {
+
+            //set counter Time
+            this.countTime = this.maxTime;
+
+            //set  element html
+            this.$timer = document.querySelector('.timer-graphic');
+
+            this.$timerTxt = document.querySelector('.timer-seconde');
+
+            this.$btnsAnswer = document.querySelectorAll('.btn-answer');
+
+            this.$containerQuestion = document.querySelector('.text-question');
+
+            //bind first question
+            this.$containerQuestion.textContent = this.questions[0].text;
+
+            //add listeners for for all buttons to  trigger answer
+            this.$btnsAnswer.forEach(($el, key) => {
+
+                //bind question  in btn
+                $el.textContent = this.questions[0].answers[key].text;
+
+                //bind event on button
+                $el.addEventListener('click', (event) => {
+
+                    //get parent
+                    const $parent = event.target.parentNode;
+
+                    //get position
+                    this.idxAnswer = Array.prototype.indexOf.call($parent.children, event.target);
+
+                    //stop timer and check response
+                    this.stop_timer().check_answer();
+                })
+            })
+
+            //set index question in quiz.indexAnswer
+
+            this.selectedQuestion = 0;
+
+            //hide loader
+            document.querySelector('.rotate-infinite').remove();
+
+            //show main
+            document.querySelector('main').style.display = "flex";
+
+
+            //start party
+            quiz.start_timer();
+
+        },
+
+        /**
+         * Get category from quiz.freefakeapi ;
+         * @return {Promise<any>}
+         */
+        getCategory: async function () {
+            let response = await fetch('https://quiz.freefakeapi.io/api/categories?random=false');
+            let categories = await response.json();
+            return categories;
+        },
+
+        /**
+         * get questions from api/questions
+         * @return {Promise<any>}
+         */
+        getQuestions: async function () {
+            const response = await fetch('https://quiz.freefakeapi.io/api/categories/' + quiz.selectedCategorie + '/questions?limit=10');
+            const questions = await response.json();
+            return questions;
+        },
+
+        /**
+         * function start timer and timer rotate
+         * @return  void
+         */
+        start_timer: function () {
+            this.refIntervalTimer = setInterval(() => {
+                this.countTime--;
+                this.deg += 360 / this.maxTime;
+                if (this.countTime >= 0) {
+                    this.timer_rotate(this.$timer, this.deg);
+                    this.$timerTxt.innerHTML = this.countTime + "S";
+                    this.animateTimer(this.deg);
+                } else {
+                    this.stop_timer().check_answer();
+                }
+            }, 1000);
+        },
+
+        /**
+         *
+         * @param deg : number of degree from $timer
+         */
+        animateTimer: function (deg) {
+            if (deg > 45 && this.deg < 90) {
+                this.$timer.style.animation = "zoom-in-zoom-level-1 0.75s ease infinite";
+            } else if (deg > 90 && deg < 180) {
+                this.$timer.style.animation = "zoom-in-zoom-level-1 0.50s ease infinite";
+            } else if (deg > 180 && deg < 225) {
+                this.$timer.style.animation = "zoom-in-zoom-level-1 0.25s ease infinite";
+            } else if (deg > 260) {
+                let timeAnimation = (360 - deg) * (0.7 / 360) + 0.1;
+                this.$timer.style.animation = "zoom-in-zoom-level-1 " + timeAnimation + "s ease infinite";
+            } else {
+                return false;
+            }
+        },
+
+        /**
+         * function stop timer & reset timer with arrow and conic position
+         * @return  this
+         */
+        stop_timer: function () {
+            clearInterval(this.refIntervalTimer);
+            this.countTime = this.maxTime;
+            this.deg = 0
+            this.timer_rotate(this.$timer, 0);
+            this.$timerTxt.innerHTML = this.maxTime + "S";
+            this.$timer.style.animation = "zoom-in-zoom-level-1 30s ease infinite";
+
+            // Retourner l'objet modifié
+            return this;
+
+        },
+
+        /**
+         * function rotate arrow timer & edit conic gradiant
+         * @param $el : HTMLElement reference of html element  timer
+         * @param deg : number
+         * @return boolean
+         */
+        timer_rotate: function ($el, deg) {
+
+            // debugger ;
+
+            let arrow = $el.querySelector('.arrow')
+            let conic = $el.querySelector(".circle")
+            arrow.style.transform = "rotate(" + deg + "deg)";
+            conic.style.backgroundImage = "repeating-conic-gradient(from 0deg,  #343434 0deg " + deg + "deg,#FFD005 " + deg + "deg 360deg )";
+        },
+
+        /**
+         * methode for check response
+         */
+        check_answer: function () {
+
+            //if timeout display badbaby
+            if (this.idxAnswer === null) {
+                this.openIframe("TIMEOUT");
+                return;
+            } else if (this.questions[this.selectedQuestion].answers[this.idxAnswer].is_correct) {
+                this.openIframe("SUCCESS");
+                return;
+            } else {
+                this.openIframe();
+            }
+
+        },
+
+        /**
+         * methode open screen bg.html || babybad.html
+         */
+        openIframe: function (page) {
+
+            let $iframe = document.querySelector('iframe');
+
+            let path;
+
+            //if  timeout open badbaby with timeout variable = true
+            if (page === "TIMEOUT") {
+                path = "badbaby.html?timeout=true&";
+            } else if (page === "SUCCESS") {
+                path = "BG.html?";
+            } else {
+                path = "badbaby.html?";
+            }
+
+
+            $iframe.src = path + '?id=' + this.selectedQuestion;
+
+            $iframe.onload = function () {
+                $iframe.style.display = "block";
+                document.querySelector('.btn-next').style.display = "block"
+            }
+
+        },
+
+        /**
+         * close iframe and set next question
+         */
+        closeIframe: function () {
+            let $iframe = document.querySelector('iframe');
+            $iframe.src = '';
+            $iframe.style.display = "none";
+            document.querySelector('.btn-next').style.display = "none";
+            $iframe.onload = null;
+
+            //next question
+            quiz.nextQuestion();
+
+
+        },
+
+        /**
+         * methode to next question
+         */
+        nextQuestion: function () {
+
+            //if we have questions again
+            if (this.selectedQuestion < 10) {
+
+                //incrementation  of selected question
+                this.selectedQuestion++
+                //bind new index question in block .nb-question
+                document.querySelector('.nb-question .value').textContent = this.selectedQuestion;
+
+                //change  to next question
+                this.$containerQuestion.textContent = this.questions[this.selectedQuestion].text;
+
+                //bind  next response
+                this.questions[this.selectedQuestion].answers.forEach((value, index) => {
+                    this.$btnsAnswer[index].textContent = value.text;
+                })
+
+                //set idxAnswer to null
+                this.idxAnswer = null;
+            } else {
+                window.location.href = 'score.html'
+            }
+
+
         }
-    },
-
-    /**
-     * function stop timer & reset timer with arrow and conic position
-     * @return  this
-     */
-    stop_timer: function () {
-        clearInterval(this.refIntervalTimer);
-        this.countTime = this.maxTime;
-        this.deg = 0
-        this.timer_rotate(this.$timer, 0);
-        this.$timerTxt.innerHTML = this.maxTime + "S";
-        this.$timer.style.animation = "zoom-in-zoom-level-1 30s ease infinite";
-
-        // Retourner l'objet modifié
-        return this;
-
-    },
-
-    /**
-     * function rotate arrow timer & edit conic gradiant
-     * @param $el : HTMLElement reference of html element  timer
-     * @param deg : number
-     * @return boolean
-     */
-    timer_rotate: function ($el, deg) {
-
-        // debugger ;
-
-        let arrow = $el.querySelector('.arrow')
-        let conic = $el.querySelector(".circle")
-        arrow.style.transform = "rotate(" + deg + "deg)";
-        conic.style.backgroundImage = "repeating-conic-gradient(from 0deg,  #343434 0deg " + deg + "deg,#FFD005 " + deg + "deg 360deg )";
-    },
-
-    /**
-     * methode for check response
-     */
-    check_answer: function () {
-
-        //if timeout display badbaby
-        if (this.idxAnswer === null) {
-            this.openIframe(0);
-            return;
-        }
-
-
-    },
-
-    /**
-     * methode
-     */
-    openIframe: function (page) {
-
-        let $iframe = document.querySelector('iframe');
-
-        //if  timeout open badbaby with timeout variable = true
-        if (page === 0) {
-            $iframe.src = 'badbaby.html?timeout=true&id=';
-            $iframe.style.display = "block";
-            return false
-        }
-
-
-    },
-
-}
-
-/**
- * Get category from cat.json ;
- * @return {Promise<any>}
- */
-async function getCategory() {
-    let response = await fetch('https://quiz.freefakeapi.io/api/categories?random=false');
-    let categories = await response.json();
-    return categories;
-};
+    }
 
 //append data category to quiz.category
-getCategory().then(apiCategories => quiz.categories = apiCategories);
+quiz.getCategory().then((apiCategories) => {
 
-//get variable get in url path
-let _get = [];
-window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-    _get[key] = value;
+    //append data api to categories quiz
+    quiz.categories = apiCategories;
+
+    //get variable get in url path
+    let _get = [];
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        _get[key] = value;
+    });
+
+    //if user no  selected category , redirect to index.html
+    if (!_get['cat'] || !quiz.categories.find(cat => cat.id === parseInt(_get['cat']))) {
+        window.location.href = 'index.html';
+    }
+    //binding cat to quiz
+    quiz.selectedCategorie = _get['cat'];
+
+    /**
+     * bind questions to quiz.questions and init quiz
+     */
+    quiz.getQuestions().then((apiQuestions) => {
+        quiz.questions = apiQuestions;
+        //init quiz
+        quiz.init();
+    }).catch(() => {
+        alert("Une erreur est survenue lors de la récupération des questions, ce n'est pas de ma faute." +
+            "Veuillez voir le problème avec #hugo");
+    })
+
+
+}).catch(() => {
+    alert("Une erreur est survenue lors de la récupération des catégories, ce n'est pas de ma faute." +
+        "Veuillez voir le problème avec #hugo");
 });
 
-//if user no  selected category , redirect to index.html
-if (!_get['cat'] ||  ! quiz.categories.find(cat => cat.id === _get[cat])) {
-     window.location.href = 'index.html';
-}
-//binding cat to quiz
-quiz.selectedCategorie = _get['cat'];
 
-
-/**
- * get questions from api/questions
- * @return {Promise<any>}
- */
-async function getQuestions() {
-    const response = await fetch('https://quiz.freefakeapi.io/api/categories/' + quiz.selectedCategorie+ '/questions?limit=10');
-    const questions = await response.json();
-    return questions;
-}
-
-/**
- * bind questions to quiz.questions
- */
-getQuestions().then(apiQuestions => quiz.questions = apiQuestions)
-
-
-console.log(quiz)
-//when  cat is loaded init quiz
-quiz.init();
 
 
 
